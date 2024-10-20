08214dbb43d35f9b036bd7458d76a837dc49035c
ykboluo <ykboluo@gmail.com>
2024-10-21 01:19:38 +0800

    chore: biome

--- app/components/app-sidebar.tsx
+++ app/components/app-sidebar.tsx
@@ -1,147 +1,153 @@
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from "~/components/ui/sidebar"
-import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "~/components/ui/collapsible"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "~/components/ui/dropdown-menu"
+	Collapsible,
+	CollapsibleContent,
+	CollapsibleTrigger,
+} from "~/components/ui/collapsible";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu";
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarGroupLabel,
+	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+} from "~/components/ui/sidebar";
 
 import {
-  LayoutDashboard,
-  Book,
-  SwatchBook,
-  Fan,
-  ChevronDown,
-  Users,
-  User2,
-  ChevronUp
-} from "lucide-react"
-
+	Book,
+	ChevronDown,
+	ChevronUp,
+	Fan,
+	LayoutDashboard,
+	SwatchBook,
+	User2,
+	Users,
+} from "lucide-react";
 
 const items = [
-  {
-    title: "Projects",
-    url: "/",
-    icon: LayoutDashboard,
-  },
-  {
-    title: "Posts",
-    href: "/post",
-    icon: Book,
-  },
-  {
-    title: "Pages",
-    href: "/page",
-    icon: SwatchBook,
-  },
-  {
-    title: "Medias",
-    href: "/media",
-    icon: Fan,
-  },
-  {
-    title: "Teams",
-    href: "/team",
-    icon: Users,
-  }
-]
+	{
+		title: "Projects",
+		url: "/",
+		icon: LayoutDashboard,
+	},
+	{
+		title: "Posts",
+		href: "/post",
+		icon: Book,
+	},
+	{
+		title: "Pages",
+		href: "/page",
+		icon: SwatchBook,
+	},
+	{
+		title: "Medias",
+		href: "/media",
+		icon: Fan,
+	},
+	{
+		title: "Teams",
+		href: "/team",
+		icon: Users,
+	},
+];
 
 export function AppSidebar() {
-  return (
-    <Sidebar>
-      <SidebarHeader>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <SidebarMenuButton>
-                  Boluo's Workspace
-                  <ChevronDown className="ml-auto" />
-                </SidebarMenuButton>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent className="w-[--radix-popper-anchor-width]">
-                <DropdownMenuItem>
-                  <span>Preferences</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <span>Workspace settings</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <span>Invite and manage members</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <span>Switch workspace</span>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarHeader>
-      <SidebarContent>
-        <Collapsible defaultOpen className="group/collapsible">
-          <SidebarGroup>
-            <SidebarGroupLabel>
-              <CollapsibleTrigger>
-                <div className="flex items-center gap-2">
-                  Workspace
-                  <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180 w-4" />
-                </div>
-              </CollapsibleTrigger>
-            </SidebarGroupLabel>
-            <CollapsibleContent>
-              <SidebarGroupContent>
-                {
-                  items.map(item => (
-                    <SidebarMenuItem key={item.title}>
-                      <SidebarMenuButton asChild>
-                        <a href={item.url}>
-                          <item.icon />
-                          <span>{item.title}</span>
-                        </a>
-                      </SidebarMenuButton>
-                    </SidebarMenuItem>
-                  ))
-                }
-              </SidebarGroupContent>
-            </CollapsibleContent>
-          </SidebarGroup>
-        </Collapsible>
-      </SidebarContent>
-      <SidebarFooter>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <SidebarMenuButton>
-                  <User2 /> Boluo
-                  <ChevronUp className="ml-auto" />
-                </SidebarMenuButton>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent
-                side="top"
-                className="w-[--radix-popper-anchor-width]"
-              >
-                <DropdownMenuItem>
-                  <span>Account</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <span>Billing</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem>
-                  <span>Sign out</span>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarFooter>
-    </Sidebar>
-  )
+	return (
+		<Sidebar>
+			<SidebarHeader>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<DropdownMenu>
+							<DropdownMenuTrigger asChild>
+								<SidebarMenuButton>
+									Boluo's Workspace
+									<ChevronDown className="ml-auto" />
+								</SidebarMenuButton>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent className="w-[--radix-popper-anchor-width]">
+								<DropdownMenuItem>
+									<span>Preferences</span>
+								</DropdownMenuItem>
+								<DropdownMenuItem>
+									<span>Workspace settings</span>
+								</DropdownMenuItem>
+								<DropdownMenuItem>
+									<span>Invite and manage members</span>
+								</DropdownMenuItem>
+								<DropdownMenuItem>
+									<span>Switch workspace</span>
+								</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarHeader>
+			<SidebarContent>
+				<Collapsible defaultOpen className="group/collapsible">
+					<SidebarGroup>
+						<SidebarGroupLabel>
+							<CollapsibleTrigger>
+								<div className="flex items-center gap-2">
+									Workspace
+									<ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180 w-4" />
+								</div>
+							</CollapsibleTrigger>
+						</SidebarGroupLabel>
+						<CollapsibleContent>
+							<SidebarGroupContent>
+								{items.map((item) => (
+									<SidebarMenuItem key={item.title}>
+										<SidebarMenuButton asChild>
+											<a href={item.url}>
+												<item.icon />
+												<span>{item.title}</span>
+											</a>
+										</SidebarMenuButton>
+									</SidebarMenuItem>
+								))}
+							</SidebarGroupContent>
+						</CollapsibleContent>
+					</SidebarGroup>
+				</Collapsible>
+			</SidebarContent>
+			<SidebarFooter>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<DropdownMenu>
+							<DropdownMenuTrigger asChild>
+								<SidebarMenuButton>
+									<User2 /> Boluo
+									<ChevronUp className="ml-auto" />
+								</SidebarMenuButton>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent
+								side="top"
+								className="w-[--radix-popper-anchor-width]"
+							>
+								<DropdownMenuItem>
+									<span>Account</span>
+								</DropdownMenuItem>
+								<DropdownMenuItem>
+									<span>Billing</span>
+								</DropdownMenuItem>
+								<DropdownMenuItem>
+									<span>Sign out</span>
+								</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarFooter>
+		</Sidebar>
+	);
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,50 +1,54 @@
 import type { MetaFunction } from "@remix-run/node";
-import { SidebarInset, SidebarProvider, SidebarTrigger } from "~/components/ui/sidebar";
 import { AppSidebar } from "~/components/app-sidebar";
+import {
+	SidebarInset,
+	SidebarProvider,
+	SidebarTrigger,
+} from "~/components/ui/sidebar";
 import { Skeleton } from "~/components/ui/skeleton";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "Tiny CMS" },
-    { name: "description", content: "Manage your content, unified" },
-  ];
+	return [
+		{ title: "Tiny CMS" },
+		{ name: "description", content: "Manage your content, unified" },
+	];
 };
 
 export default function Index() {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-          <SidebarTrigger className="-ml-1" />
-        </header>
-        <main className="flex flex-1 flex-col gap-4 p-4">
-          <div className="grid auto-rows-min gap-12 md:grid-cols-3">
-            <div className="flex flex-col space-y-3">
-              <Skeleton className="h-[200px] rounded-xl" />
-              <div className="space-y-2">
-                <Skeleton className="h-4" />
-                <Skeleton className="h-4" />
-              </div>
-            </div>
-            <div className="flex flex-col space-y-3">
-              <Skeleton className="h-[200px] rounded-xl" />
-              <div className="space-y-2">
-                <Skeleton className="h-4" />
-                <Skeleton className="h-4" />
-              </div>
-            </div>
-            <div className="flex flex-col space-y-3">
-              <Skeleton className="h-[200px] rounded-xl" />
-              <div className="space-y-2">
-                <Skeleton className="h-4" />
-                <Skeleton className="h-4" />
-              </div>
-            </div>
-          </div>
-          <div className="min-h-[100vh] flex-1 rounded-xl bg-muted md:min-h-min" />
-        </main>
-      </SidebarInset>
-    </SidebarProvider>
-  );
+	return (
+		<SidebarProvider>
+			<AppSidebar />
+			<SidebarInset>
+				<header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+					<SidebarTrigger className="-ml-1" />
+				</header>
+				<main className="flex flex-1 flex-col gap-4 p-4">
+					<div className="grid auto-rows-min gap-12 md:grid-cols-3">
+						<div className="flex flex-col space-y-3">
+							<Skeleton className="h-[200px] rounded-xl" />
+							<div className="space-y-2">
+								<Skeleton className="h-4" />
+								<Skeleton className="h-4" />
+							</div>
+						</div>
+						<div className="flex flex-col space-y-3">
+							<Skeleton className="h-[200px] rounded-xl" />
+							<div className="space-y-2">
+								<Skeleton className="h-4" />
+								<Skeleton className="h-4" />
+							</div>
+						</div>
+						<div className="flex flex-col space-y-3">
+							<Skeleton className="h-[200px] rounded-xl" />
+							<div className="space-y-2">
+								<Skeleton className="h-4" />
+								<Skeleton className="h-4" />
+							</div>
+						</div>
+					</div>
+					<div className="min-h-[100vh] flex-1 rounded-xl bg-muted md:min-h-min" />
+				</main>
+			</SidebarInset>
+		</SidebarProvider>
+	);
 }
